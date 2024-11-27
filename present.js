1d6fb8ff1cc0efadeb21c306049ce948513868d9
Jordan <jlu79754@gmail.com>
2024-11-27 13:50:44 -0500

    Initial commit

---
+++ present.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    const present = document.getElementById('present');
+
+    // Change the image and redirect on click
+    present.addEventListener('click', () => {
+        present.src = 'opened-present.png'; // Replace with the open present image
+        setTimeout(() => {
+            window.location.href = 'index.html'; // Redirect to the main page
+        }, 1000); // Wait 1 second before redirecting
+    });
+};
